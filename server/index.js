a48b504d3bcffbdfcc221be82bea21efda7eb78d
solomon <odumahs@gmail.com>
2021-01-30 15:54:22 +0100

    add documentation

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import logger from "morgan";
 import bodyParser from "body-parser";
 import index from "./routes";
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 require("express-async-errors");
 
@@ -9,6 +11,10 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 app.set("port", port);
+
+// API Docs
+const swaggerDocument = YAML.load(`${process.cwd()}/server/swagger.yaml`);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(logger("dev"));
 
